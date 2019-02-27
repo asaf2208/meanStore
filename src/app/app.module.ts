2a5db8979654585e2fb4335561309748725304cd
asaf <theasf0@gmail.com>
2019-02-27 11:41:15 +0200

    adde imports for forms module and new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from './products/products.component';
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BranchesComponent } from './branches/branches.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,13 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     AboutComponent,
     BranchesComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { 
@@ -42,6 +46,10 @@ import { FooterComponent } from './footer/footer.component';
       { 
         path: 'about',
         component: AboutComponent
+      },
+      { 
+        path: 'login',
+        component: LoginComponent
       },
       { 
         path: '**',
