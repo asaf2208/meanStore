437701615c9b0378a74bc05c2ffdf0d5b02cca63
asaf <theasf0@gmail.com>
2019-02-27 13:00:57 +0200

    replaced login component with signup component

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+ 
+  loginForm: FormGroup;
+
+
+  constructor(fb: FormBuilder,private http: HttpClient,     private router: Router,  private route: ActivatedRoute) {
+    this.loginForm = fb.group({
+      email: ['',[Validators.required,Validators.email]],
+      username: ['',Validators.required],
+      password: ['',Validators.required],
+      fullname: ['',Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  };
+
+  get username() {
+    return this.loginForm.get('username');
+  };
+
+  get password() {
+    return this.loginForm.get('password');
+  };  
+
+  get fullname() {
+    return this.loginForm.get('fullname');
+  };
+
+  onSubmit(evt) {
+    console.log(1);
+
+    console.log(this.password.value);
+
+      this.http.post<any>('http://localhost:3000/users/adduser',
+      {
+        "firstname": this.username.value,
+        "password": this.password.value,
+        "fullname": this.fullname.value,
+        "email": this.email.value
+      }).subscribe((data) => {
+          console.log(data);
+          if(data.message === "Created user successfully") {
+            this.router.navigateByUrl("/products");
+          }
+      });
+
+
+  }
+
+
+}
